ccd3fcb5e54d4209a8271ff690fbf55fe19d5779
MadaE <madalina.enuc@yahoo.com>
2018-05-03 13:26:40 +0300

    first commit

---
+++ mhotel/web/src/main/java/hotel/rest/CustomerResource.java
@@ -0,0 +1,45 @@
+package hotel.rest;
+
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+
+import hotel.business.CustomerService;
+import mhotel.model.Customer;
+
+@Path("/customers")
+public class CustomerResource {
+	private static final Logger __logger = Logger.getLogger("hotel.rest.CustomerResource");
+
+	@GET
+	@Path("/all")
+	@Produces("application/json")
+	public List<Customer> getAllCustomers() throws Exception {
+		if (__logger.isLoggable(Level.INFO))
+			__logger.log(Level.INFO, "getAllCustomers entered");
+		CustomerService cs = new CustomerService();
+		return cs.getAllCustomers();
+	}
+
+	@GET
+	@Path("/all/{cid}")
+	public Customer getCustomerById(@PathParam("cid") long pId) throws Exception {
+		return new CustomerService().getCustomerById(pId);
+	}
+
+	@POST
+	@Path("/add")
+	@Consumes("application/json")
+	@Produces("application/json")
+	public Customer addCustomer(Customer pCustomer) throws Exception {
+		return new CustomerService().addCustomer(pCustomer);
+	}
+}
